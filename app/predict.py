69125ef2ed45772a694e4f0af2f6f409fb28d263
piotrb <piotrbanko@me.com>
2024-02-25 22:29:56 +0100

    Add predict.py: make predictions with the trained model

---
+++ app/predict.py
@@ -0,0 +1,65 @@
+"""Make predictions using the model"""
+import numpy as np
+import torch
+import albumentations as A
+from PIL import Image
+import timm
+
+from imagesloader.imagesloader import ImagesLoader
+from albumentations.pytorch import ToTensorV2
+
+
+class Predictor:
+    def __init__(self, model, model_path):
+        self.model = model
+        self.model.load_state_dict(torch.load(model_path))
+
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        print(f"Using device: {self.device}")
+
+        self.model = self.model.to(self.device)
+
+    def predict(self, image_path, transform):
+        self.model.eval()  # Set the model to evaluation mode
+
+        # Open the image file and apply transformations
+        with Image.open(image_path) as img:
+            input_tensor = transform(image=np.array(img))
+            input_batch = input_tensor['image'].unsqueeze(0)  # Create a mini-batch as expected by the model
+
+        # Move the input and model to GPU for speed if available
+        input_batch = input_batch.to(self.device)
+
+        # Convert the input tensor to FloatTensor
+        input_batch = input_batch.float()
+
+        with torch.no_grad():
+            # Make a prediction
+            output = self.model(input_batch)
+
+        # The output has unnormalized scores. To get probabilities run a softmax on it
+        probabilities = torch.nn.functional.softmax(output[0], dim=0)
+
+        return probabilities
+
+
+if __name__ == "__main__":
+    loader = ImagesLoader()
+    loader.get_data('../data/google_api_images')
+    n_classes = loader.get_number_of_classes()
+    image_size = 600
+
+    backbone = 'resnet18'
+    model = timm.create_model(backbone,
+                              pretrained=True,
+                              num_classes=n_classes)
+
+    predictor = Predictor(model, "../data/models/model.pth")
+
+    transform = A.Compose([A.Resize(image_size, image_size),
+                           ToTensorV2()])
+
+    probabilities = predictor.predict("../data/google_api_images/Grzegorzecka+3_180.jpg", transform)
+    print(probabilities)
+
+    print(f"Predicted class: {np.argmax(probabilities.cpu().numpy())}")
