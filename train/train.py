023a06cd242efda4304606bdf260953b5d6d2b4e
piotrb <piotrbanko@me.com>
2024-02-25 19:52:55 +0100

    Refactor train.py

--- train/train.py
+++ train/train.py
@@ -71,7 +71,8 @@ class Trainer:
         self.scheduler = scheduler
         self.train_dataloader = train_dataloader
         self.validate_dataloader = validate_dataloader
-        self.seed = seed
+
+        set_seed(seed)
 
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         print(f"Using device: {self.device}")
@@ -98,8 +99,6 @@ class Trainer:
             with torch.set_grad_enabled(True):
                 # Forward: Get model outputs
                 y_pred = self.model(X)
-
-                # print(y_pred)
 
                 # Forward: Calculate loss
                 loss = self.criterion(y_pred, y.long())
@@ -175,8 +174,6 @@ class Trainer:
         for epoch in range(epochs):
             print(f"Epoch {epoch + 1}/{epochs}")
 
-            set_seed(self.seed + epoch)
-
             acc, loss = self.train_one_epoch()
 
             if self.validate_dataloader:
@@ -232,22 +229,7 @@ def set_seed(seed=98):
     torch.backends.cudnn.benchmark = False
 
 
-if __name__ == '__main__':
-    # Load and prepare the data
-    loader = ImagesLoader()
-    loader.get_data('../data/google_api_images')
-    df = loader.data
-
-    # Transform for the training dataset
-    image_size = 600
-    transform_soft = A.Compose([A.Rotate(p=0.6, limit=(-30, 30), crop_border=True),
-                                A.Resize(image_size, image_size),
-                                A.HorizontalFlip(p=0.6),
-                                A.CoarseDropout(max_holes=1, max_height=64, max_width=64, p=0.3),
-                                ToTensorV2()])
-
-    n_folds = 5
-
+def check_model(df, image_size, batch_size, learning_rate, lr_min, epochs, backbone, n_folds, transform):
     # Create a new column for cross-validation folds
     df["kfold"] = -1
 
@@ -258,31 +240,23 @@ if __name__ == '__main__':
     for fold, (train_, val_) in enumerate(skf.split(X=df, y=df.label)):
         df.loc[val_, "kfold"] = fold
 
+    final_acc_list = []
+    final_loss_list = []
     for fold in range(n_folds):
+        print(f"Fold: {fold}")
         train_df = df[df.kfold != fold].reset_index(drop=True)
         validate_df = df[df.kfold == fold].reset_index(drop=True)
 
-        # train_df, test_df = train_test_split(df, test_size=0.2, random_state=98)
-        # train_df, validate_df = train_test_split(train_df, test_size=0.2, random_state=98)
-
-        train_dataset = CustomDataset(train_df, image_size, transform=transform_soft)
+        train_dataset = CustomDataset(train_df, image_size, transform=transform)
         validate_dataset = CustomDataset(validate_df, image_size)
 
-        batch_size = 16
         train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
         validate_dataloader = DataLoader(validate_dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
-        # Crate the model (use pretrained models made by Ross Wightman)
-
         n_classes = loader.get_number_of_classes()
         print(f'Total classes: {n_classes}')
-        backbone = 'resnet18'
 
-        learning_rate = 0.0001
-
-        lr_min = 1e-5
-        epochs = 15
-
+        # Crate the model (use pretrained models made by Ross Wightman)
         model = timm.create_model(backbone,
                                   pretrained=True,
                                   num_classes=n_classes)
@@ -308,3 +282,35 @@ if __name__ == '__main__':
 
         trainer.visualize_history(acc, loss, val_acc, val_loss)
 
+        final_acc = val_acc[-1]
+        final_loss = val_loss[-1]
+
+        final_acc_list.append(final_acc)
+        final_loss_list.append(final_loss)
+
+    print(f"Final mean val accuracy: {np.mean(final_acc_list)}")
+    print(f"Final mean val loss: {np.mean(final_loss_list)}")
+
+
+if __name__ == '__main__':
+    # Load and prepare the data
+    loader = ImagesLoader()
+    loader.get_data('../data/google_api_images')
+    df = loader.data
+
+    image_size = 600
+    batch_size = 16
+    learning_rate = 0.0001
+    lr_min = 1e-5
+    epochs = 2
+    backbone = 'resnet18'
+    n_folds = 5
+
+    # Transform for the training dataset
+    transform = A.Compose([A.Rotate(p=0.6, limit=(-30, 30), crop_border=True),
+                           A.Resize(image_size, image_size),
+                           A.HorizontalFlip(p=0.6),
+                           A.CoarseDropout(max_holes=2, max_height=64, max_width=64, p=0.3),
+                           ToTensorV2()])
+
+    check_model(df, image_size, batch_size, learning_rate, lr_min, epochs, backbone, n_folds, transform)
